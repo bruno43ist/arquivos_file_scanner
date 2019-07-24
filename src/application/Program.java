e427d8c13e236a2143f87c124b685d828a7e1085
Bruno Farias <bruno43ist@yahoo.com.br>
2019-07-24 19:48:28 -0300

    Folders

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,36 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+		Scanner sc = new Scanner(System.in);
 		
-		String path = "/Users/brunofarias/Desktop/out.txt";//CAMINHO DO ARQUIVO
-	
-		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
-			for(String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-		} catch(IOException e) {
-			e.printStackTrace();
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		File[] folders = path.listFiles(File::isDirectory);//lista pastas
+		
+		for(File foulder : folders) {
+			System.out.println(foulder);
 		}
+		
+		System.out.println();
+		
+		File[] files = path.listFiles(File::isFile); //lista arquivos 
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "/criadoEclipse").mkdir();
+		System.out.println("Directory created! " + success);
+
+		
+		sc.close();
 	}
 
 }
