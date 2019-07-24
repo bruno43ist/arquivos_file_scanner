5d1c201d9c9196368b4f7d9e71b4de4ddcb707d5
Bruno Farias <bruno43ist@yahoo.com.br>
2019-07-24 19:39:57 -0300

    FileWriter e BufferedWriter

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,26 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		String path = "/Users/brunofarias/Desktop/in.txt";//CAMINHO DO ARQUIVO
+		String[] lines = new String[] {"Good Morning", "Good Afternoon", "Good Night"};
+		
+		String path = "/Users/brunofarias/Desktop/out.txt";//CAMINHO DO ARQUIVO
 	
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			String line = br.readLine();
-			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 		} catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} 
+			e.printStackTrace();
+		}
 	}
 
 }
