96a67e3aed5e809cb1b35ca251766036b03c586b
Bruno Farias <bruno43ist@yahoo.com.br>
2019-07-24 19:51:52 -0300

    information about file path

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,25 +9,14 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter a folder path: ");
-		String strPath = sc.nextLine();
+		System.out.println("Enter a file path: ");
+		String strPath =  sc.nextLine();
 		
 		File path = new File(strPath);
-		File[] folders = path.listFiles(File::isDirectory);//lista pastas
 		
-		for(File foulder : folders) {
-			System.out.println(foulder);
-		}
-		
-		System.out.println();
-		
-		File[] files = path.listFiles(File::isFile); //lista arquivos 
-		for(File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPath + "/criadoEclipse").mkdir();
-		System.out.println("Directory created! " + success);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 
 		
 		sc.close();
